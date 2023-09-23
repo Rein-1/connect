7ed7f58dcc700cd73bc5e2fcd374845135f673ad
Rein-1 <markreniermercado123@gmail.com>
2023-09-24 03:56:21 +0800

    modified homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Row, Col, Button } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap';
-import './Home.css'
+import './Home.css';
 
 function Home() {
-  return <Row>
+  return ( 
+  <Row>
     <Col md={6} className='d-flex flex-direction-column align-item-center justify-content-center'>
         <div>
-            <h1>Share your thoughts with me</h1>
-            <p>Chat App will let you connent with me</p>
+            <h1>Share your thoughts with with people</h1>
+            <p>Connectichat connects people</p>
             <LinkContainer to="/chat">
                 <Button variant='success'>
                     Get Started <i className='fas fa-comments home-message-icon'></i>
@@ -17,8 +18,9 @@ function Home() {
             </LinkContainer>
         </div>
     </Col>
-    <Col md={6} className='home-bg'></Col>
+    <Col md={6} className='home__bg'></Col>
   </Row>
+  );
 }
 
 export default Home
